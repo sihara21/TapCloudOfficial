9877ea52957cd935de1505a298c92af4b737d1d5
sihara21 <147967440+sihara21@users.noreply.github.com>
2025-04-10 22:24:04 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to TapCloud WebApp</h1>
+      <p>This is a basic template. Your WebApp is working!</p>
+    </div>
+  )
+}
+
+export default App
